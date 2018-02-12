49683b0b869e95e4587e2d4e929b628b17e953a7
KristofferK <kristoffer@minlejebolig.dk>
2018-02-12 14:50:44 +0100

    Add another caching strategy
    Added RigidEducationCachingStrategy. Data from within 12 hours will remain in cache. Data older than 12 hours will not.

---
+++ EducationScraperSolution/ClassLibrary1/Library/Caching/CachingStrategies/RigidEducationCachingStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EducationScrapers.Models;
+
+namespace EducationScrapers.Library.Caching.CachingStrategies
+{
+    public class RigidEducationCachingStrategy : IEducationCachingStrategy
+    {
+        public bool KeepInCache(Education education)
+        {
+            TimeSpan diff = DateTime.Now - education.DateFetched;
+            return diff.TotalHours < 12;
+        }
+    }
+}
